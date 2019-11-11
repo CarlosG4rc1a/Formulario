4ec7904698229df9a110352d9bce046f1c3383ec
CarlosG4rc1a <cgarcia@ifp.mx>
2019-11-11 13:27:24 -0600

    (Primer commit)

---
+++ Code.js
@@ -0,0 +1,14 @@
+//Triggered when the page is navigated to, serves up HTML
+function doGet(){
+ var template = HtmlService.createTemplateFromFile('index');
+ return template.evaluate().setTitle('Example App').setSandboxMode(HtmlService.SandboxMode.IFRAME);
+}
+//Called from the client with form data, basic validation for blank values
+function formSubmit(formData){
+ for(var field in formData){
+ if(formData[field] == ''){
+ return {success: false, message: field + ' Cannot be blank'}
+ }
+ }
+ return {success: true, message: 'Sucessfully submitted!'};
+}
